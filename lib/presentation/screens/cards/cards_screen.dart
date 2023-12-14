3665ea426738c3712f0cfc3847f6fb5a600bb708
cazabe <cazcarlos@hotmail.com>
2023-12-14 12:02:01 -0500

    Finishing cards

--- lib/presentation/screens/cards/cards_screen.dart
+++ lib/presentation/screens/cards/cards_screen.dart
@@ -30,21 +30,38 @@ class _CardsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ...cards.map(
-          (card) => _CardTypeOne(
-            label: card['label'],
-            elevation: card['elevation`'],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ...cards.map(
+            (card) => _CardTypeOne(
+              label: card['label'],
+              elevation: card['elevation'],
+            ),
           ),
-        ),
-        ...cards.map(
-          (card) => _CardTypeTwo(
-            label: card['label'],
-            elevation: card['elevation`'],
+          ...cards.map(
+            (card) => _CardTypeTwo(
+              label: card['label'],
+              elevation: card['elevation'],
+            ),
           ),
-        ),
-      ],
+          ...cards.map(
+            (card) => _CardTypethree(
+              label: card['label'],
+              elevation: card['elevation'],
+            ),
+          ),
+          ...cards.map(
+            (card) => _CardTypeFour(
+              label: card['label'],
+              elevation: card['elevation'],
+            ),
+          ),
+          const SizedBox(
+            height: 100,
+          ),
+        ],
+      ),
     );
   }
 }
@@ -61,22 +78,20 @@ class _CardTypeOne extends StatelessWidget {
       child: Padding(
         padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
         //Single child scroll view helps when widgets overflow
-        child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Align(
-                alignment: Alignment.topRight,
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(Icons.more_vert_outlined),
-                ),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert_outlined),
               ),
-              Align(
-                alignment: Alignment.bottomLeft,
-                child: Text(label),
-              )
-            ],
-          ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(label),
+            )
+          ],
         ),
       ),
     );
@@ -116,6 +131,79 @@ class _CardTypeTwo extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class _CardTypethree extends StatelessWidget {
+  const _CardTypethree({required this.label, required this.elevation});
+  final String label;
+  final double elevation;
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Theme.of(context).colorScheme;
+    return Card(
+      color: color.surface,
+      elevation: elevation,
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(10, 5, 10, 10),
+        //Single child scroll view helps when widgets overflow
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.topRight,
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert_outlined),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(label),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _CardTypeFour extends StatelessWidget {
+  const _CardTypeFour({required this.label, required this.elevation});
+  final String label;
+  final double elevation;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      //this one is like clicpRrec make cool borders
+      clipBehavior: Clip.hardEdge,
+      elevation: elevation,
+      child: Stack(
+        children: [
+          Image.network(
+            'https://picsum.photos/id/${elevation.toInt()}/600/250',
+            height: 350,
+            fit: BoxFit.cover,
+          ),
+          Align(
+            alignment: Alignment.topRight,
+            child: Container(
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(20),
+                ),
+              ),
+              child: IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.more_vert_outlined),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
